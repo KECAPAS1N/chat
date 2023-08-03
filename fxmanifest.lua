02f01e462e80c934c04f71e6ed7d25e37a24d1f5
MARFY <118571529+M4RFY@users.noreply.github.com>
2023-08-04 01:27:21 +0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -23,14 +23,6 @@ files {
   'ui/vendor/vue.2.3.3.min.js',
   'ui/vendor/flexboxgrid.6.3.1.min.css',
   'ui/vendor/animate.3.5.2.min.css',
-  'ui/vendor/latofonts.css',
-  'ui/vendor/fonts/LatoRegular.woff2',
-  'ui/vendor/fonts/LatoRegular2.woff2',
-  'ui/vendor/fonts/LatoLight2.woff2',
-  'ui/vendor/fonts/LatoLight.woff2',
-  'ui/vendor/fonts/LatoBold.woff2',
-  'ui/vendor/fonts/LatoBold2.woff2',
-  'ui/vendor/fonts/*.ttf'
 }
 
 lua54 'yes'
